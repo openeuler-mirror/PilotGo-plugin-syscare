864dcc84a86f87744d4cbc17c71f339fb320d678
zhanghan2021 <zhanghan@kylinos.cn>
2024-04-03 09:32:32 +0800

    get run command and result to post server in agent

---
+++ app/agent/controller/run.go
@@ -0,0 +1,49 @@
+package controller
+
+import (
+	"gitee.com/openeuler/PilotGo-plugin-syscare/agent/config"
+	"gitee.com/openeuler/PilotGo-plugin-syscare/agent/service"
+	"gitee.com/openeuler/PilotGo/sdk/logger"
+	"gitee.com/openeuler/PilotGo/sdk/response"
+	"github.com/gin-gonic/gin"
+)
+
+func RunCommandHandler(c *gin.Context) {
+	w := &struct {
+		TaskId           string   `json:"taskId"`
+		Ip               string   `json:"ip"`
+		BuildKernelSrc   string   `json:"buildKernel"`
+		BuildDebugInfo   string   `json:"buildDebugInfo"`
+		PatchDescription string   `json:"patchDescription"`
+		PatchVersion     string   `json:"patchVersion"`
+		PatchRelease     string   `json:"patchRelease"`
+		PatchType        string   `json:"patchType"`
+		Patchs           []string `json:"patchs"`
+	}{}
+	if err := c.BindJSON(w); err != nil {
+		response.Fail(c, nil, err.Error())
+		return
+	}
+	workDir := config.Config().Storage.Work + w.TaskId // 创建工作目录
+	if err := service.MakeDir(workDir); err != nil {
+		response.Fail(c, nil, err.Error())
+		return
+	}
+	for i, patch := range w.Patchs {
+		w.Patchs[i] = workDir + "/" + patch
+		if err := service.DownloadPatchsFromServer(patch, w.TaskId, workDir); err != nil { // 从服务器中下载patch到工作目录
+			logger.Error("保存patch文件%v失败: %v", patch, err.Error())
+			continue
+		}
+	}
+
+	sourceDir := config.Config().Storage.Path + w.BuildKernelSrc
+	debugInfoDir := config.Config().Storage.Path + w.BuildDebugInfo
+	args := service.CommandSplice(sourceDir, debugInfoDir, w.PatchDescription, w.PatchVersion, w.PatchRelease, w.PatchType, w.Patchs)
+	err := service.RunCommand(w.TaskId, w.Ip, workDir, args)
+	if err != nil {
+		response.Fail(c, nil, err.Error())
+		return
+	}
+	response.Success(c, nil, "执行结束")
+}
---
+++ app/agent/service/run.go
@@ -0,0 +1,98 @@
+package service
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"gitee.com/openeuler/PilotGo-plugin-syscare/agent/client"
+	"gitee.com/openeuler/PilotGo-plugin-syscare/utils"
+	"gitee.com/openeuler/PilotGo/sdk/logger"
+	"gitee.com/openeuler/PilotGo/sdk/utils/httputils"
+)
+
+func RunCommand(taskId, ip, workDir string, args []string) error {
+	var buildLog string
+	var kernelSrcRpm string
+	var kernelRpm string
+
+	exitCode, stdout, _, err := utils.RunCommand("cd "+workDir+" && "+"kylin-warm build", args...)
+	logger.Info("%v", stdout)
+	if err != nil {
+		logger.Error("执行热补丁命令失败：%v", err.Error())
+		kernelRpm = ""
+		kernelSrcRpm = ""
+	}
+	kernelSrcRpm, kernelRpm, err = ScanHotPatchRpm(workDir)
+	if err != nil {
+		logger.Error("搜索agent制作成功的热补丁包失败: %v", err.Error())
+	}
+
+	if exitCode != 0 { // 制作热补丁失败，读取build.log
+		buildLog, err = GetBuildLog(workDir)
+		if err != nil {
+			logger.Error("读取build.log日志失败:%v", err.Error())
+		}
+	}
+
+	data := &struct {
+		TaskId       string `json:"taskId"`
+		IP           string `json:"ip"`
+		KernelSrcRpm string `json:"kernelSrcRpm"`
+		KernelRpm    string `json:"kernelRpm"`
+		RetCode      int    `json:"retcode"`
+		Stdout       string `json:"stdout"`
+		BuildLog     string `json:"buildLog"`
+	}{
+		TaskId:       taskId,
+		IP:           ip,
+		KernelSrcRpm: kernelSrcRpm,
+		KernelRpm:    kernelRpm,
+		RetCode:      exitCode,
+		Stdout:       stdout,
+		BuildLog:     buildLog,
+	}
+	url := "http://" + client.GlobalClient.AgentServer + "/plugin/syscare/scriptResult"
+	resp, err := httputils.Post(url, &httputils.Params{
+		Body: data,
+	})
+	if err != nil {
+		return err
+	}
+
+	d := &struct {
+		Code    int    `json:"code"`
+		Message string `json:"msg"`
+	}{}
+	err = json.Unmarshal(resp.Body, d)
+	if err != nil {
+		logger.Error("unmarshal send result error: %s", err.Error())
+		return err
+	}
+	if d.Code != http.StatusOK {
+		return errors.New(d.Message)
+	}
+	return nil
+}
+func CommandSplice(buildKernel, buildDebugInfo, patchDescription, patchVersion, patchRelease, patchType string, patchs []string) []string {
+	args := []string{}
+
+	args = append(args, "--source", buildKernel)
+	args = append(args, "--debuginfo", buildDebugInfo)
+	args = append(args, "--patch-name", patchType)
+	args = append(args, "--patch")
+	args = append(args, patchs...)
+
+	if patchDescription != "" {
+		args = append(args, "--patch-description", patchDescription)
+	}
+	if patchVersion != "" {
+		args = append(args, "--patch-version", patchVersion)
+	}
+	if patchRelease != "" {
+		args = append(args, "--patch-release", patchRelease)
+	}
+
+	logger.Info("run command args: %v", args)
+	return args
+}
